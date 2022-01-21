743f99433b5ab212acc39695db4b15b933a81aa4
Martin <mdsugarev@gmail.com>
2022-01-21 12:57:32 +0200

    a

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
 
-
 export default function Logo({type}){
 
 
 return(
 
-type === 'default' || '' ? <img src="./images/logo.svg"/> : <img src="./images/logo-muted.svg" />
+type === 'default' ? <img src="./images/logo.svg"/> : <img src="./images/logo-muted.svg" />
 
 )
 
+
+}
+
+Logo.defaultProps = {
+    type: 'default'
 }
