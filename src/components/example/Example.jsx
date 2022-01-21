713c006f253a292e2d8914136f9c03feaa420f0b
Martin <mdsugarev@gmail.com>
2022-01-21 10:56:00 +0200

    add logo

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -1,6 +1,6 @@
 import Paper from "@mui/material/Paper";
 import Container from "@mui/material/Container";
-import Link from "../link/Link";
+import Logo from "../logo/Logo.jsx";
 import styles from "./Example.module.scss";
 import classNames from "classnames";
 
@@ -8,11 +8,7 @@ export default function Copyright() {
   return (
     <div className={classNames(styles.wrapper)}>
       <Container className={classNames(styles.container)} maxWidth="xl">
-        <Paper className={classNames(styles.paper)}>
-          <Link href="/about" color="secondary">
-            Hi
-          </Link>
-        </Paper>
+          <Logo type={'default'} /> 
       </Container>
     </div>
   );
