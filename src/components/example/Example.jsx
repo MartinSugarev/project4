53d752f84eface27442b9e6d1dce2cf6c2abf5a6
Martin <mdsugarev@gmail.com>
2022-01-21 23:07:13 +0200

    a

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -1,15 +1,27 @@
-import Paper from "@mui/material/Paper";
-import Container from "@mui/material/Container";
+import { Grid, Button, Input } from "@mui/material";
 import Logo from "../logo/Logo.jsx";
 import styles from "./Example.module.scss";
-import classNames from "classnames";
+import SearchIcon from '@mui/icons-material/Search';
+
 
 export default function Copyright() {
   return (
-    <div className={classNames(styles.wrapper)}>
-      <Container className={classNames(styles.container)} maxWidth="xl">
-          <Logo type={'default'} /> 
-      </Container>
-    </div>
+    
+  <div className={styles.wrapper}>
+    <Grid container  className={styles.container}>
+      <Grid item>
+          <Logo type={'default'} className={styles.logo}/> 
+      </Grid>
+      <Grid item >
+          <Input startAdornment={<SearchIcon/>} className={styles.input} placeholder="Find items, users and activities" />
+      </Grid>
+      <Grid item >
+      <Button variant="text">Outlined</Button>
+      <Button variant="text">Outlined</Button>
+      <Button variant="outlined">Outlined</Button>
+      </Grid>
+    </Grid>
+  </div>
+         
   );
 }
