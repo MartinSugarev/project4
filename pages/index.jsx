a4c7a907eb5ac38d9fc94861fa1725f59f6c687d
Martin <mdsugarev@gmail.com>
2022-01-22 19:25:17 +0200

    k

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,10 @@
-import Header from "../src/components/header/Header";
+//import Header from "../src/components/header/Header";
 import Avatar from "../src/components/avatar/Avatar"
 
 
 
 export default function Index() {
-  return <Avatar size={90}  url={require('../src/components/avatar/image/avatar.png')} verified={false} />
+  return <Avatar url={require('../src/components/avatar/image/avatar.png')} />
 
   
 }
