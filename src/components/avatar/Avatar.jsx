c091fbe2c22c94dac7b78d08497798c844017ad8
Martin <mdsugarev@gmail.com>
2022-01-22 22:06:50 +0200

    a

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -2,19 +2,13 @@ import styles from './Avatar.module.scss'
 import classNames from 'classnames'
 
 
-export default function Avatar({size, url, verified}){
+export default function Avatar({size = 90, url, verified = false}){
  
 return (
-    <div className={styles.avatar} style={{width: size + 'px', height: size + 'px'}} >
+    <div className={styles.avatar} style={{width: size + 'px' || '90px', height: size + 'px' || '90px'}} >
         <img className={styles.image}  src={url.default.src}/>
        {verified === true ?  <img src="/Users/macbookair/Desktop/project4/public/images/verified.svg" className={styles.badge}/> : ''}
     </div>
 )
 
-}
-
-
-Avatar.defaultProps = {
-    size: 90,
-    verified: false
 }
