a8a35dc71494aed6f72b52aabcd32db03a8d8a93
Martin <mdsugarev@gmail.com>
2022-01-23 00:29:42 +0200

    c

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,7 @@ import Avatar from '../src/components/avatar/Avatar'
 
 export default function Index() {
   return (
-    <Avatar  url={require('../public/images/avatar.png')} />
+    <Avatar size={false} verified={true}  url={require('../public/images/avatar.png')} />
   )
 
   
