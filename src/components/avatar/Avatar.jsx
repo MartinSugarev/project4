bdd768ec3186047b9ea133e752625569d027605b
Martin <mdsugarev@gmail.com>
2022-01-22 20:51:32 +0200

    q

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -7,9 +7,19 @@ const cl = classNames.bind(styles)
 export default function Avatar({size, url, verified}){
  
 return (
-    <div className={cl('avatar')} style={{width: size + 'px', height: size + 'px'}} >
-        <img className={cl('image')}  src={url.default.src}/>
-       {verified === true ?  <img src="./image/verified.svg" className={cl('badge')} />  : ''}
+    <div className={cl('avatar')} style={{width: size + 'px', height: size + 'px', display: 'grid',
+    position: 'relative',    
+    borderStyle: 'dashed',
+    borderRadius: '10px',
+    borderColor: '#4e24f2',
+    placeItems: 'center',
+    borderWidth: '3px'}} >
+        <img className={cl('image')}  src={url.default.src} style={{width: '100%', borderRadius: '50%'}}/>
+       {verified === true ?  <img src="./image/verified.svg" className={cl('badge')} style={{
+           position: 'absolute',
+           bottom: '12%',
+           left: '65%'
+       }} />  : ''}
     </div>
 )
 
