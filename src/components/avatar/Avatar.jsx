69e3f258e58db66de9d00fd5ee63f7947c4b6f43
Martin <mdsugarev@gmail.com>
2022-01-23 14:05:00 +0200

    f

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -6,11 +6,13 @@ const badge = require('../../../public/images/verified.svg')
 export default function Avatar({size = 90, url = require('../../../public/images/avatar.png'), verified = false}){
  console.log(badge)
 return (
+    <>
     <div className={styles.avatar} style={{width: size + 'px', height: size + 'px'}} >
         <img className={styles.image}  src={url.default.src} alt="Avatar"/>
-           <img className={styles.badge}  src={badge.default.src} alt="Badge"  />
-
+        <img className={styles.badge}  src={badge.default.src} alt="Badge"  />
     </div>
+    <div className="badge"></div>
+    </>
 )
 
 }
