dfaaf80b547ca9246eab904db8e7c58285edb5da
Martin <mdsugarev@gmail.com>
2022-01-23 22:44:34 +0200

    ggg

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -7,10 +7,13 @@ import badge from  '../../../public/images/verified.svg'
 
 export default function Avatar({size = 90, url, verified = false}){
 return (
+    <>
    <div className={styles.avatar} style={{width: size + 'px', height: size + 'px'}} >
         
        { verified ? <div className={styles.badge}> <img style={{width: '25px'}} src={badge.src} /></div>   : <div className={styles.badge}><img  src={badge.src} style={{display: 'none'}}/></div>  }
    </div>
+   <img className={styles.image}  src={url.default.src} alt="Avatar"/>
+   </>
 );
 };
 
