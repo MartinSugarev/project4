bb4dba6ec3cdcdbbdb5342c204903934c8a0ba82
Martin <mdsugarev@gmail.com>
2022-01-23 23:45:45 +0200

    kok

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
+import { Avatar as A } from "@mui/material"
 import styles from './Avatar.module.scss'
 import badge from  '../../../public/images/verified.svg'
 import cl from 'classnames'
-import { Avatar as AV } from "@mui/material"
+
 
 
 
 export default function Avatar({size = 90, url, verified = false}){
 return (
     
-   <div className={cl(styles.avatar)} style={{width: size + 'px', height: size + 'px'}} >
-        <AV
- className={cl(styles.avatar)}
-  alt="Remy Sharp"
-  src={url.default.src}
-  sx={{ width: '100%', height: '100%'}} />
-       { verified ? <div className={cl(styles.badge)}> <img style={{width: '25px'}} src={badge.src} /></div>   : <div className={cl(styles.badge)}><img  src={badge.src} style={{display: 'none'}}/></div>  }
-   </div>
+   <A className={cl(styles.avatar)} style={{width: size + 'px', height: size + 'px'}} >
+        <img className={cl(styles.image)}  src={url.default.src} alt="Avatar"/>
+       { verified ? <div className={cl(styles.badge)}> <img style={{width: '25px'}} src={badge.src} /></div>   : <div className={cl(styles.badge)}> <img  src={badge.src} style={{display: 'none'}}/></div>  }
+   </A>
 );
 };
 
