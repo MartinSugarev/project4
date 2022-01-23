5334e4606e9d87109450c6f4c633c0165d82294b
Martin <mdsugarev@gmail.com>
2022-01-23 23:08:12 +0200

    d

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import styles from './Avatar.module.scss'
 import badge from  '../../../public/images/verified.svg'
 
@@ -8,10 +8,13 @@ import badge from  '../../../public/images/verified.svg'
 
 export default function Avatar({size = 90, url, verified = false}){
 return (
-   <div className={styles.avatar} style={{width: size + 'px', height: size + 'px'}} >
-        <img className={styles.image}  src={url.default.src} alt="Avatar"/>
-       { verified ? <div className={styles.badge}> <img style={{width: '25px'}} src={badge.src} /></div>   : <div className={styles.badge}><img  src={badge.src} style={{display: 'none'}}/></div>  }
+   <div className={styles.wrapper}>
+      <div className={styles.avatar} style={{width: size + 'px', height: size + 'px'}} >  
    </div>
+   <img className="image"  src={url.default.src} alt="Avatar"/>
+   { verified ? <div className={styles.badge}> <img style={{width: '25px'}} src={badge.src} /></div>   : <div className={styles.badge}><img  src={badge.src} style={{display: 'none'}}/></div>  }
+   </div> 
+   
 );
 };
 
