46aace66485e206a2b6b24b8726cfbaad23d92cd
Martin <mdsugarev@gmail.com>
2022-01-23 20:58:49 +0200

    ggg

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,13 +1,15 @@
 import styles from './Avatar.module.scss'
-const badge = require('../../../public/images/verified.svg')
-
+import badge from  '../../../public/images/verified.svg'
 export default function Avatar({size = 90, url, verified = false}){
+
 
 return (
     <>
     <div className={styles.avatar} style={{width: size + 'px', height: size + 'px'}} >
         <img className={styles.image}  src={url.default.src} alt="Avatar"/>
-        <img className={styles.badge}  src={badge.default.src} alt="Badge"  />
+        <span className={styles.badge}>
+          <img src={badge.src} style={{width: '25px', height: '25px'}}/>
+        </span>
     </div>
     </>
 )
