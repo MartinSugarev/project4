7dd70934bb38e91f2d86ed61c895b504c273c6bf
Martin <mdsugarev@gmail.com>
2022-01-24 00:04:01 +0200

    rer

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Avatar as A } from "@mui/material"
 import styles from './Avatar.module.scss'
 import badge from  '../../../public/images/verified.svg'
 import cl from 'classnames'
@@ -10,10 +9,10 @@ import cl from 'classnames'
 export default function Avatar({size = 90, url, verified = false}){
 return (
     
-   <A className={cl(styles.avatar)} style={{width: size + 'px', height: size + 'px'}} >
-        <img className={cl(styles.image)}  src={url.default.src} alt="Avatar"/>
+   <div className={styles.avatar} style={{width: size + 'px', height: size + 'px'}} >
+        <img className={cl(styles.image)}  src={url.default.src} alt="Image"/>
        { verified ? <div className={cl(styles.badge)}> <img style={{width: '25px'}} src={badge.src} /></div>   : <div className={cl(styles.badge)}> <img  src={badge.src} style={{display: 'none'}}/></div>  }
-   </A>
+   </div>
 );
 };
 
