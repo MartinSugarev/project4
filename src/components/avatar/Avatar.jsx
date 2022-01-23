155c0746cfd3300f2fc45100e82a0e9a55bf340f
Martin <mdsugarev@gmail.com>
2022-01-23 21:06:31 +0200

    ff

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -3,16 +3,12 @@ import badge from  '../../../public/images/verified.svg'
 export default function Avatar({size = 90, url, verified = false}){
 
 
-return (
-    <>
-    <div className={styles.avatar} style={{width: size + 'px', height: size + 'px'}} >
+return <div className={styles.avatar} style={{width: size + 'px', height: size + 'px'}} >
         <img className={styles.image}  src={url.default.src} alt="Avatar"/>
-        <span className={styles.badge}>
-          <img src={badge.src} style={{width: '25px', height: '25px'}}/>
-        </span>
-    </div>
-    </>
-)
+       <img className={styles.badge} src={badge.src} />
+   </div>
+
+
 
 }
 
