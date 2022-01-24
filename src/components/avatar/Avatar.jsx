b5f1ddbd45a4f2638c53012adaa4ec06981cdcf4
Martin <mdsugarev@gmail.com>
2022-01-24 10:36:39 +0200

    gggggg

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -9,9 +9,13 @@ import badge from  '../../../public/images/verified.svg'
 export default function Avatar({size = 90, url, verified = false}){
 return (
     
-   <div  style={{width: size + 'px', height: size + 'px'}} >
-        <img className={styles.image}  src={url.default.src} alt="Image"/>
-       { verified ? <div className={styles.badge}> <img style={{width: '25px'}} src={badge.src} /></div>   : <div className={styles.badge}> <img  src={badge.src} style={{display: 'none'}}/></div>  }
+   <div className="avatar" style={{width: size + 'px', height: size + 'px'}} >
+        <img className="image" style={{width: '100%', height: '100%', borderRadius: '50%'}}  src={url.default.src} alt="Image"/>
+       { verified ? <div className="badge"> <img style={{width: '25px',position: 'absolute',
+bottom: '0',
+left: '65%',
+width: '25px',
+height: '25px'}} src={badge.src} /></div>   : <div className="badge"> <img   src={badge.src} style={{display: 'none'}}/></div>  }
    </div>
 );
 };
