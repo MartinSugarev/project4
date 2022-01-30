2d7e1cd1d72e6e56aca532932da497effb886818
Martin <mdsugarev@gmail.com>
2022-01-30 16:19:23 +0200

    qq

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,60 +1,9 @@
 import Trending from '../src/components/trending/Trending'
 
-const obj = {
-  "cards":[
-     {
-        "name":"Ivy",
-        "user":{
-           "avatar":{
-              "url":"images/avatar.png"
-           },
-           "verified":true
-        },
-        "mediaUrl":"images/nft.jpg",
-        "price":1,
-        "currency":"ETH"
-     },
-     {
-        "name":"Judie",
-        "user":{
-           "avatar":{
-              "url":"images/avatar.png"
-           },
-           "verified":true
-        },
-        "mediaUrl":"images/nft.jpg",
-        "price":2.3,
-        "currency":"ETH"
-     },
-     {
-        "name":"Juniper",
-        "user":{
-           "avatar":{
-              "url":"images/avatar.png"
-           },
-           "verified":true
-        },
-        "mediaUrl":"images/nft.jpg",
-        "price":5,
-        "currency":"ETH"
-     },
-     {
-        "name":"Maple",
-        "user":{
-           "avatar":{
-              "url":"images/avatar.png"
-           },
-           "verified":false
-        },
-        "mediaUrl":"images/nft.jpg",
-        "price":10,
-        "currency":"ETH"
-     }
-  ]
-}
+
 
 export default function Index() {
-  return  <Trending cards={obj} />
+  return  <Trending  />
   
 
 }
